11b9ed8f4763e899faff58e6a57f7f4badd73180
Gabriel Leon <gleon@recurly.com>
2017-12-20 00:05:50 -0700

    added forming team and creating game functionality using combo of routing and redux push to browser history now need to in the redirected to page start the game

--- src/components/interactivePlayer/interactivePlayerContainer.js
+++ src/components/interactivePlayer/interactivePlayerContainer.js
@@ -4,17 +4,39 @@ import * as actions from '../../actions';
 import Searcher from '../common/searcher';
 
 export class InteractivePlayerContainer extends Component {
+  renderTeam(team) {
+    return team ? team.map(member => { return member.name }).join(', '): '';
+  }
+
   render() {
     const style = {
       top: '100px',
       position: 'relative',
     };
+    const searchContainer = {
+      height: '300px',
+      overflow: 'scroll',
+    }
     return (
       <div style={style}>
-        <Searcher searchingFor='game' />
+        <h2>Your team so far is: {this.renderTeam(this.props.team)}</h2>
+        <div style={searchContainer}>
+          <strong>First pick your team:</strong>
+          <Searcher searchingFor='user' />
+        </div>
+        <div style={searchContainer}>
+          <strong>After you have your team, pick a game:</strong>
+          <Searcher searchingFor='game' />
+        </div>
       </div>
     );
   };
 };
 
-export default connect(null)(InteractivePlayerContainer);
+function mapStateToProps (state) {
+  return {
+    team: state.app.latestTeam,
+  };
+};
+
+export default connect(mapStateToProps)(InteractivePlayerContainer);
